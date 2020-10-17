2abe3fd9c186fe76fc6f6003f105a780742cf8e9
Vo Lap <knvolap@gmail.com>
2020-10-17 16:47:41 +0700

    Initial commit

---
+++ app/src/main/java/VoLap/example/circleview/MainActivity.java
@@ -0,0 +1,21 @@
+package VoLap.example.circleview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+
+import com.example.circleview.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
